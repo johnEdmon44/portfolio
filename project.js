fe651e27de2a24da678119010daa82bb620ddb03
Nessaj44 <johnedmoncoc@gmail.com>
2024-02-10 19:11:25 +0800

    Add project.js

---
+++ project.js
@@ -0,0 +1,138 @@
+const projects = [
+  {
+    id:  1,
+    name: "Manga browser",
+    description: "Manga Infinity is a web application designed for manga enthusiasts. " +
+                 "It provides a platform where users can browse through a vast collection of manga, categorized by genre, " +
+                 "popularity, and recency. Users can also search for specific manga titles and add them to their personal " +
+                 "bookmark list for easy access. The application leverages the Jikan API to fetch manga data and uses Redux " +
+                 "for state management",
+    live: "https://github.com/johnEdmon44/manga-infinity",
+    repository: "https://github.com/johnEdmon44/manga-infinity",
+    skills: [
+      {
+        name: "HTML",
+        img: "./images/html.png",
+      },
+      {
+        name: "CSS",
+        img: "./images/css.png",
+      },  
+      {
+        name: "Tailwind",
+        img: "./images/tailwind.png",
+      }, 
+      {
+        name: "React",
+        img: "./images/tailwind.png",
+      }
+    ],
+    
+  },
+  {
+    id:  2,
+    name: "COVID-19 Tracker",
+    description: `This project focuses on developing a COVID-19 tracker or API integration. It aims to provide real-time data and statistics related to the COVID-19 pandemic,
+                 such as the number of cases, deaths, recoveries, and other relevant information. The project may involve fetching data from reputable sources, implementing data visualization through charts or graphs, allowing users to select specific countries or regions for data retrieval,
+                 and providing a user-friendly interface to access and interpret the COVID-19 data. The objective is to keep users informed about the global or regional impact of the pandemic and help them make informed decisions based on the available data.`,
+    live: "https://johnedmon44.github.io/covid-tracker/",
+    repository: "https://github.com/johnEdmon44/covid-tracker",
+    skills: [
+      {
+        name: "HTML",
+        img: "./images/html.png",
+      },
+      {
+        name: "CSS",
+        img: "./images/css.png",
+      },
+      {
+        name: "JavaScript",
+        img: "./images/javascript.png",
+      },
+      {
+        name: "React",
+        img: "./images/react.png",
+      }
+    ],
+  },
+  {
+    id:   3,
+    name: "Shopping Cart",
+    description: `This project involves creating a shopping cart that allows users to browse products, add items to their cart,  
+                 and checkout securely. It requires designing a visually appealing interface with HTML and CSS, implementing functionality with JavaScript,  
+                 and utilizing React for efficient development. The end result is a user-friendly and dynamic web application.`,
+    skills: [
+      {
+        name: "HTML",
+        img: "./images/html.png",
+      },
+      {
+        name: "CSS",
+        img: "./images/css.png",
+      },
+      {
+        name: "JavaScript",
+        img: "./images/javascript.png",
+      },
+      {
+        name: "React",
+        img: "./images/react.png",
+      }
+    ],
+    liveDemoLink: "https://main--precious-cranachan-eb0b22.netlify.app/shop",
+    sourceCodeLink: "https://github.com/johnEdmon44/shopping-cart",
+  }, 
+  {
+    id:   4,
+    name: "Typing Test",
+    description: `This typing test project allows users to practice their typing skills by typing randomly generated words within a time limit.  
+                 It features an intuitive interface with options for difficulty and time duration selection, a countdown timer,  
+                 word display, input field, start and end buttons, score tracking, and a leaderboard. The project utilizes HTML, CSS, and
+                 JavaScript for implementation, with local storage for leaderboard persistence. It provides an interactive and engaging platform for users to
+                 improve their typing speed and accuracy.`,
+    skills: [
+      {
+        name: "HTML",
+        img: "./images/html.png",
+      },
+      {
+        name: "CSS",
+        img: "./images/css.png",
+      },
+      {
+        name: "JavaScript",
+        img: "./images/javascript.png",
+      }
+    ],
+    liveDemoLink: "https://johnedmon44.github.io/typing-test/",
+    sourceCodeLink: "https://github.com/johnEdmon44/typing-test",
+  },
+  {
+    id:   5,
+    name: "Multi-Step Form",
+    description: `The Multi-Step Form project is a user-friendly web application designed to enhance the user experience when collecting information,  
+                 making selections, or guiding users through a series of steps. This project offers a modern and intuitive approach to forms that  
+                 simplifies data input and decision-making, making it ideal for a wide range of applications, from user registrations to product selections.`,
+    skills: [
+      {
+        name: "HTML",
+        img: "./images/html.png",
+      },
+      {
+        name: "CSS",
+        img: "./images/css.png",
+      },
+      {
+        name: "JavaScript",
+        img: "./images/javascript.png",
+      },
+      {
+        name: "React",
+        img: "./images/react.png",
+      }
+    ],
+    liveDemoLink: "https://johnedmon44.github.io/multi-step-form/",
+    sourceCodeLink: "https://github.com/johnEdmon44/multi-step-form",
+  }
+];
